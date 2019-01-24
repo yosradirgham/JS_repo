949a37c563bbd15ece39c699d76084f535693426
yosradirgham <dirgham.yosra@gmail.com>
2019-01-24 13:41:54 +0100

    commit

--- weather-app/weather.js
+++ weather-app/weather.js
@@ -3,9 +3,9 @@ console.log('----------------------------starting weather.js--------------------
 
 const request = require('request');
 
-var getWeather = (lattitude, longitude, callback)=> {
+var getWeather = (latitude, longitude, callback)=> {
 	request({
-		url :'https://api.darksky.net/forecast/5c3e5f7558451d60d503027352a124f9/39.944267,-75.163609'
+		url :`https://api.darksky.net/forecast/5c3e5f7558451d60d503027352a124f9/${latitude},${-75.163609}`
 	}, (error, response, body)=>{
 		if(error) callback('cannot connect to the forecast server', undefined);
 		else{
