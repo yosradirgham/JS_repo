afe1b15a0e890ab584914b692d8f782c9a305555
yosradirgham <dirgham.yosra@gmail.com>
2019-02-16 12:43:06 +0100

    set mongoose model

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -6,19 +6,50 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo',{
   text:{
-    type:String
+    type      : String,
+    required  : true,
+    minlength : 1,
+    trim      : true
   },
   completed:{
-    type:Boolean
+    type    : Boolean,
+    default : false
   },
   completedAt:{
+    type    : Number,
+    default : null
+  }
+});
+
+var User = mongoose.model('User',{
+  name:{
+    type:String,
+  },
+  age:{
     type:Number
+  },
+  location:{
+    type:String
+  },
+  email:{
+    type: String,
+    require: true,
+    trim:true
   }
 });
 
 var newTodo = new Todo({
-  text:'Cook dinner'
+  text:'   Finish this course   '
 });
 
-newTodo.save().then(doc => console.log(doc))
+var newUser = new User({
+  email:'yosra@example.com'
+});
+
+newTodo.save()
+.then(doc => console.log(doc))
 .catch(e => console.log(e));
+
+newUser.save()
+.then(doc => console.log(`user saved:\n${doc}`))
+.catch(e => console.log(`Unable to save user:\n${e}`));
