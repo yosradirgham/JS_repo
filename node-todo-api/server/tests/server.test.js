65c9244a5ab45f90976ac79706168a64996070ce
yosradirgham <dirgham.yosra@gmail.com>
2019-02-16 18:07:36 +0100

    test POST /todos route

---
+++ node-todo-api/server/tests/server.test.js
@@ -0,0 +1,62 @@
+const expect    = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+
+const {mongoose} = require('../db/mongoose');
+const {Todo} = require('./../models/todo');
+
+
+beforeEach((done)=>{
+  Todo.remove({})
+  .then(()=>done());
+});
+
+
+describe('Post /todos',()=>{
+
+  describe('todo created',()=>{
+    it('Should create a new todo',(done)=>{
+      var text = 'Finish this course';
+      request(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect(res=>{
+        expect(res.body.text).toBe(text);
+      })
+      .end((err,res)=>{
+        if(err) return done(err);
+        Todo.find().then(todos => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch(e =>{
+          done(e);
+        });
+      });
+    });
+  });
+
+  describe('correct data sent',()=>{
+    it('Should not create todo with invalid body data',(done)=>{
+
+      request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err,res)=>{
+        if(err) return done(err);
+        Todo.find().then(todos =>{
+          console.log(todos.length);
+          expect(todos.length).toBe(0);
+          done();
+        }).catch(e=>{
+          done(e);
+        });
+      });
+    });
+  });
+
+
+});
