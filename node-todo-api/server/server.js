7c60c4c22a0fdbeb78b9d92de776bc50f8908c75
yosradirgham <dirgham.yosra@gmail.com>
2019-02-16 15:01:41 +0100

    Add POST /todos route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,22 +1,35 @@
-const {mongoose} = require('./db/mongoose')
+//Todo rest API
 
+const express    = require('express');
+const bodyParser = require('body-parser');//parses json and converts it into a JS obj
+
+const {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-const express = require('express');
+var app = express();
 
-// var newTodo = new Todo({
-//   text:'   Finish this course   '
-// });
-//
-// var newUser = new User({
-//   email:'yosra@example.com'
-// });
-//
-// newTodo.save()
-// .then(doc => console.log(doc))
-// .catch(e => console.log(e));
-//
-// newUser.save()
-// .then(doc => console.log(`user saved:\n${doc}`))
-// .catch(e => console.log(`Unable to save user:\n${e}`));
+// Middlewares configuration
+app.use(bodyParser.json());// With this in place we can send json to the server
+//the body will get stored by body-parser <3
+
+// Data manipulation
+app.post('/todos',(req,res,err)=>{
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  // save our todo in our database
+  todo.save()
+  .then(doc => console.log(`document added to database:\n${res.send(doc)}`))
+  .catch(e => console.log(`Unable to add todo:\n${res.status(400).send(e)}`));
+});
+
+app.post('/users',(req,res,err)=>{
+
+});
+
+
+app.listen(3000,()=>{
+  console.log('Started on port 3000');
+});
